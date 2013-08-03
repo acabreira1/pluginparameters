6bd26c0c5b90b70cb3cd28d9e3f08fa7c58f1898
4drX <contact.me@the.juce.forum>
2013-08-03 15:00:34 +0200

    update

--- search/functions_72.js
+++ search/functions_72.js
@@ -1,9 +1,11 @@
 var searchData=
 [
   ['read',['read',['../class_presets.html#a6d2cee1772e62f5259d4a86a9e17b21e',1,'Presets']]],
-  ['remove',['remove',['../class_presets.html#a1f9c4a4bd47b7cd9beb431504c5ae46c',1,'Presets']]],
+  ['removelastloaded',['removeLastLoaded',['../class_presets.html#ad72337c5f85bedb18f1c3d0fbc57a112',1,'Presets']]],
+  ['removelastselected',['removeLastSelected',['../class_presets.html#abe30e774dd128dc13f1beef095b15843',1,'Presets']]],
   ['removexmlfile',['removeXmlFile',['../class_param_group.html#ade01223a1debadfd640a6d02abdf839f',1,'ParamGroup']]],
   ['rename',['rename',['../class_presets.html#ad1dcf362f9120bed51f651980d1a1dab',1,'Presets']]],
+  ['renamexmlfile',['renameXmlFile',['../class_param_group.html#ae7f04a5d47de812cebc22aaf22c48fae',1,'ParamGroup']]],
   ['reread',['reread',['../class_presets.html#afaf148bdf00753cae3d26eeedd53c4bb',1,'Presets']]],
   ['reset',['reset',['../class_presets.html#ad20897c5c8bd47f5d4005989bead0e55',1,'Presets']]],
   ['resetupdatefromflag',['resetUpdateFromFlag',['../class_param.html#ad2698c4b06111e0d015116f7f72077fa',1,'Param']]],
