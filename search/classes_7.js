820fb8a3d723a7faaee9e8a7e7830fd242d3f9ce
4drX <contact.me@the.juce.forum>
2015-02-08 13:48:24 +0100

    update

--- search/classes_7.js
+++ search/classes_7.js
@@ -4,5 +4,15 @@ var searchData=
   ['typeparam_3c_20bool_20_3e',['TypeParam&lt; bool &gt;',['../class_type_param.html',1,'']]],
   ['typeparam_3c_20floattype_20_3e',['TypeParam&lt; FloatType &gt;',['../class_type_param.html',1,'']]],
   ['typeparam_3c_20inttype_20_3e',['TypeParam&lt; IntType &gt;',['../class_type_param.html',1,'']]],
-  ['typeparam_3c_20string_20_3e',['TypeParam&lt; String &gt;',['../class_type_param.html',1,'']]]
+  ['typeparam_3c_20string_20_3e',['TypeParam&lt; String &gt;',['../class_type_param.html',1,'']]],
+  ['typeparamarray',['TypeParamArray',['../class_type_param_array.html',1,'']]],
+  ['typeparamarray_3c_20bool_20_3e',['TypeParamArray&lt; bool &gt;',['../class_type_param_array.html',1,'']]],
+  ['typeparamarray_3c_20floattype_20_3e',['TypeParamArray&lt; FloatType &gt;',['../class_type_param_array.html',1,'']]],
+  ['typeparamarray_3c_20inttype_20_3e',['TypeParamArray&lt; IntType &gt;',['../class_type_param_array.html',1,'']]],
+  ['typeparamarray_3c_20string_20_3e',['TypeParamArray&lt; String &gt;',['../class_type_param_array.html',1,'']]],
+  ['typeparammatrix',['TypeParamMatrix',['../class_type_param_matrix.html',1,'']]],
+  ['typeparammatrix_3c_20bool_20_3e',['TypeParamMatrix&lt; bool &gt;',['../class_type_param_matrix.html',1,'']]],
+  ['typeparammatrix_3c_20floattype_20_3e',['TypeParamMatrix&lt; FloatType &gt;',['../class_type_param_matrix.html',1,'']]],
+  ['typeparammatrix_3c_20inttype_20_3e',['TypeParamMatrix&lt; IntType &gt;',['../class_type_param_matrix.html',1,'']]],
+  ['typeparammatrix_3c_20string_20_3e',['TypeParamMatrix&lt; String &gt;',['../class_type_param_matrix.html',1,'']]]
 ];
